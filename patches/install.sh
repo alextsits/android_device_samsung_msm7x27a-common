37d7190a606c7ccb1ac22aef86b53c41888f22fe
bleached <alexhs.tsi@gmail.com>
2014-03-21 22:05:16 +0200

    [bluetooth] Change to bluedroid [2/3]
    Change-Id: I0db66f10730b622e878eb77ffcc4508c9f38e221

--- patches/install.sh
+++ patches/install.sh
@@ -9,23 +9,7 @@ cd frameworks/native
 echo "Applying frameworks/native patches..."
 git am $rootdirectory/device/samsung/msm7x27a-common/patches/frameworks_native/*.patch
 cd $rootdirectory
-cd build
-echo "Applying build patches..."
-git am $rootdirectory/device/samsung/msm7x27a-common/patches/build/*.patch
-cd $rootdirectory
-cd packages/apps/Settings
-echo "Applying packages/apps/Settings patches..."
-git am $rootdirectory/device/samsung/msm7x27a-common/patches/packages_apps_settings/*.patch
-cd $rootdirectory
 cd system/core
 echo "Applying system/core patches..."
 git am $rootdirectory/device/samsung/msm7x27a-common/patches/system_core/*.patch
 cd $rootdirectory
-cd hardware/libhardware_legacy
-echo "Applying hardware/libhardware_legacy patches..."
-git am $rootdirectory/device/samsung/msm7x27a-common/patches/libhardware_legacy/*.patch
-cd $rootdirectory
-#cd vendor/tmobile/apps/ThemeChooser
-#echo "Applying vendor/tmobile/apps/ThemeChooser patches..."
-#git am $rootdirectory/device/samsung/msm7x27a-common/patches/vendor_tmobile_apps_ThemeChooser/*.patch
-#cd $rootdirectory
