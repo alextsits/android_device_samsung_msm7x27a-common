66e07fe15bf6ecd8d1ca5326ea12d898e819ad48
bleached <alexhs.tsi@gmail.com>
2014-07-04 10:34:32 +0300

    Fix patches and overlay settings.

--- patches/install.sh
+++ patches/install.sh
@@ -3,7 +3,7 @@ rootdirectory="$PWD"
 cd $rootdirectory
 cd frameworks/base
 echo "Applying frameworks/base patches..."
-git am $rootdirectory/device/samsung/msm7x27a-common/patches/frameworks_base/*.patch
+git apply --whitespace=fix $rootdirectory/device/samsung/msm7x27a-common/patches/frameworks_base/*.patch
 cd $rootdirectory
 cd frameworks/native
 echo "Applying frameworks/native patches..."
@@ -11,5 +11,17 @@ git am $rootdirectory/device/samsung/msm7x27a-common/patches/frameworks_native/*
 cd $rootdirectory
 cd system/core
 echo "Applying system/core patches..."
-git am $rootdirectory/device/samsung/msm7x27a-common/patches/system_core/*.patch
+git apply --whitespace=fix $rootdirectory/device/samsung/msm7x27a-common/patches/system_core/*.patch
+cd $rootdirectory
+cd packages/apps/ROMControl
+echo "Cherry-picking in ROMControl to fix libs..."
+git cherry-pick af3d08da #fix libs
+git cherry-pick 48dc333652b868b9434fb0029420ebcfb5d9c8b6 #fix cs translations
+git cherry-pick 0bf9d4a8f71b25f146dacd8562449666acecb648 #fix hu translations
+git cherry-pick 4cf500c2836708d5c4b0d8842b3a3c2cc1d21e16 #remove mGerrit dependency
+cd $rootdirectory
+cd vendor/aokp
+echo "Cherry-picking in vendor/aokp..."
+git cherry-pick 3dd4ee37b1bb2100bd3e832f8ca0bf3eada7d026 #add mGerrit prebuilt
+git checkout ff6583fc1ce24e7117dad2fbfe09bb784d9536d4 --theirs -- prebuilt/common/app/mGerrit.apk  #bump mGerrit to v2.07
 cd $rootdirectory
